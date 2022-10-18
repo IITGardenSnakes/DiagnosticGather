4f0fafd70cae84f0813274282383cb4a518e3d6c
radfre <Schatzalex97@gmail.com>
2022-10-17 23:46:11 -0400

    made stuff

--- take.sh
+++ take.sh
@@ -1,1 +1,13 @@
-sudo fswebcam -r 720x720  ./image111.jpg
+#! /bin/bash
+
+echo moving old image to replay folder
+NOW=$( date '+%F_%H:%M:%s' )
+echo $NOW
+mkdir /home/gardenPi/DiagnosticGather/replay
+sudo mv /home/gardenPi/ImageHost/images/Cplant.jpg "/home/gardenPi/DiagnosticGather/replay/GardenPiReplay$NOW.jpg"
+
+
+{ sudo fswebcam -r 720x720  ../ImageHost/images/Cplant.jpg; } || { sudo raspistill -o ../ImageHost/images/Cplant.jpg; }
+
+
+
